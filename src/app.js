18888ec274730cdb17bb66a7a5bd8d9de5f8d026
pavel shevrin <77666606+Shevrin@users.noreply.github.com>
2021-11-23 22:09:12 +0300

    edit with prettier, add counSelect method

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import './style.css';
+import React from "react";
+import "./style.css";
 
 /**
  * Приложение
@@ -7,38 +7,48 @@ import './style.css';
  */
 
 function App({ store }) {
-
-	return (
-		<div className='App'>
-			<div className='App__head'>
-				<h1>Приложение на чистом JS</h1>
-			</div>
-			<div className='Controls'>
-				<button onClick={() => store.createItem()}> Добавить</button>
-			</div>
-			<div className='App__center'>
-				<div className='List'>{store.getState().items.map(item =>
-					<div
-						key={item.code}
-						className={'List__item' + (item.selected ? ' List__item_selected' : '')}
-					>
-						<div className='Item' onClick={() => store.selectItem(item.code)}>
-							<div className='Item__number'>{item.code}</div>
-							<div className='Item__title'>{item.title}
-								<span className={!item.count ? 'Item__span' : ''}> | Выделялся {item.count} раз</span>
-							</div>
-							<div className='Item__actions'>
-								<button onClick={() => store.deleteItem(item.code)}>
-									Удалить
-								</button>
-							</div>
-						</div>
-					</div>
-				)}
-				</div>
-			</div>
-		</div>
-	);
+  function selectItemOnClick(item) {
+    store.selectItem(item.code);
+    store.countSelectItem(item.code);
+  }
+  return (
+    <div className="App">
+      <div className="App__head">
+        <h1>Приложение на чистом JS</h1>
+      </div>
+      <div className="Controls">
+        <button onClick={() => store.createItem()}> Добавить</button>
+      </div>
+      <div className="App__center">
+        <div className="List">
+          {store.getState().items.map((item) => (
+            <div
+              key={item.code}
+              className={
+                "List__item" + (item.selected ? " List__item_selected" : "")
+              }
+            >
+              <div className="Item" onClick={() => selectItemOnClick(item)}>
+                <div className="Item__number">{item.code}</div>
+                <div className="Item__title">
+                  {item.title}
+                  <span className={!item.count ? "Item__span" : ""}>
+                    {" "}
+                    | Выделялся {item.count} раз
+                  </span>
+                </div>
+                <div className="Item__actions">
+                  <button onClick={() => store.deleteItem(item.code)}>
+                    Удалить
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,65 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Store from './store.js';
-import App from './app.js';
+import React from "react";
+import ReactDOM from "react-dom";
+import Store from "./store.js";
+import App from "./app.js";
 
 const root = document.getElementById("app");
 
 // Состояние приложения
 const store = new Store({
-	items: [{
-		code: 1, title: 'Название элемента', count: ''
-	}
-
-		,
-	{
-		code: 2, title: 'Некий объект', count: ''
-	}
-
-		,
-	{
-		code: 3, title: 'Заголовок', count: ''
-	}
-
-		,
-	{
-		code: 4, title: 'Короткое название', count: ''
-	}
-
-		,
-	{
-		code: 5, title: 'Запись', count: ''
-	}
-
-		,
-	{
-		code: 6, title: 'Пример названия', count: ''
-	}
-
-		,
-	{
-		code: 7, title: 'Седьмой', count: ''
-	}
-
-	]
-}
-
-);
+  items: [
+    { code: 1, title: "Название элемента" },
+    { code: 2, title: "Некий объект" },
+    { code: 3, title: "Заголовок" },
+    { code: 4, title: "Короткое название" },
+    { code: 5, title: "Запись" },
+    { code: 6, title: "Пример названия" },
+    { code: 7, title: "Седьмой" },
+  ],
+});
 
 // Сообщаем реакту что и куда рендерить.
 store.subscribe(() => {
-	ReactDOM.render(<App store={
-		store
-	}
-
-	/>, root);
-}
-
-);
+  ReactDOM.render(<App store={store} />, root);
+});
 
 // Сообщаем реакту что и куда рендерить.
-ReactDOM.render(<App store={
-	store
-}
-
-/>, root);
+ReactDOM.render(<App store={store} />, root);
